870967c464487c53b57aa38f6798a1361bc5714b
Juyel Hossain <luxlang.org@gmail.com>
2026-05-07 18:04:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,26 @@
 const CACHE = 'ncwph-v2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/app.js',
-  '/ncwph_engine.wasm',
-  '/manifest.json'
+    '/',
+    '/index.html',
+    '/app.js',
+    '/ncwph_engine.js',
+    '/ncwph_engine.wasm',
+    '/manifest.json',
+    '/icon.svg'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
-  self.skipWaiting();
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-    ))
-  );
-  self.clients.claim();
+    e.waitUntil(caches.keys().then(keys => 
+        Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ));
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
+    e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
 });
